2151c65732c09aa51fd3bfc9b27526b1e52a5edd
Rohan <43006609+rohanramesh38@users.noreply.github.com>
2024-03-22 15:26:51 -0400

    Updated data_extractor.py

--- data_extractor.py
+++ data_extractor.py
@@ -2,38 +2,65 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
-import wget
+import io
+import os
 import numpy as np
 from fuzzywuzzy import fuzz
+import logging
+import PyPDF2
+import requests
+from io import BytesIO
+from collections import defaultdict 
+import nest_asyncio
+nest_asyncio.apply()
+import json
+from llama_parse import LlamaParse
 
 
+# Configuring logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='log_file.log', encoding='utf-8', level=logging.INFO)
+
+
+# Initializing LlamaParse object
+parser = LlamaParse(
+    api_key=os.getenv('llama_parse_key'),  
+    result_type="markdown", 
+    num_workers=4, 
+    verbose=True,
+    language="en" 
+)
+
+
+
+# Function to get DOM object from URL
 def get_dom_object(url,HEADERS):
-
-  response = requests.get(url,HEADERS)
-  #print(url,response.status_code )
+  response = requests.get(url,headers=HEADERS)
   soup = BeautifulSoup(response.content, "html.parser")
   dom = etree.HTML(str(soup))
-
+  logger.info(str(soup))
   return dom,(response.status_code == 200)
 
-def download_pdf(url, save_path):
-    response = requests.get(url)
-    with open(save_path, 'wb') as pdf_file:
-        pdf_file.write(response.content)
 
+# Function to get JSON object from URL
+def get_json_object(url,HEADERS):
+  response = requests.get(url,headers=HEADERS)
+  json_obj=json.loads(response.content)
+  return json_obj,(response.status_code == 200)
+
+# This function extracts specific data from an HTML document (DOM) using XPath queries
 def get_data_with_xpath(dom,xpath,company_code,base_url):
-
   part=None
   report_url=""
   is_sumplimental=False
 
   if(company_code=="MGS"):
     element_list=dom.xpath(xpath)
+    
     if(element_list):
       element=element_list[0]
       part=element.attrib["href"]
       report_url=base_url+part
-      #print(report_url)
       return report_url,(part!=None)
 
   if(company_code=="BOFA"):
@@ -42,7 +69,6 @@ def get_data_with_xpath(dom,xpath,company_code,base_url):
     if(element_list):
       element=element_list[0]
       report_url=element.attrib["href"]
-      #print(etree.tostring(element))
       text='random'
       try:
         text=element.attrib["aria-label"]
@@ -54,9 +80,9 @@ def get_data_with_xpath(dom,xpath,company_code,base_url):
       return report_url,is_sumplimental
 
 
-def get_company_data(dom,comany,current_year):
+def get_company_data(dom,comany,current_year,is_json_req=False):
 
-  data={}
+  data=defaultdict(lambda: []) 
   if(comany["Code"]=="MGS"):
     for year in range(comany["report_avalaible_from"],comany["report_avalaible_till"]-1,-1):
         data[year]=[]
@@ -95,39 +121,82 @@ def get_company_data(dom,comany,current_year):
               obj["isAvailabe"]=True
               data[int(year)].append(obj)
               break
+          
+  if(comany["Code"]=="JPM"):
+    if(is_json_req):
+      obj={}
+      for value in dom["items"]:
+        year=value['year'].strip()
+        
+        if(obj):
+          data[int(year)].append(obj)
+        
+        obj={}  
+
+        quarter=value['quarter'][0]
+        obj["Quarter"]=quarter
+        obj["FinancialSuppliments"]=""
+        obj["isAvailabe"]=False
+
+        try:
+          link=value["docs"]["supplement"]["link"]
+          obj["FinancialSuppliments"]=comany["base_url"]+link
+          obj["isAvailabe"]=True
+
+        except:
+          logger.info("Error")
 
   return data
 
-company_list=[
 
+company_list=[
              {   "Name":"Morgan Stanley",
                  "Code":"MGS",
                  "report_avalaible_from":2023,
                  "report_avalaible_till":2002,
                  "url":"https://www.morganstanley.com/about-us-ir/earnings-releases",
-                 "base_url":"https://www.morganstanley.com"
+                 "base_url":"https://www.morganstanley.com",
+                 "json_req":False
              },
               {  "Name":"Bank of America",
                  "Code":"BOFA",
-                 "report_avalaible_from":2023,
-                 "report_avalaible_till":2018,
+                 "report_avalaible_from":2018,
+                 "report_avalaible_till":2023,
                  "url":"https://investor.bankofamerica.com/quarterly-earnings",
-                 "base_url":"https://investor.bankofamerica.com/quarterly-earnings"
+                 "base_url":"https://investor.bankofamerica.com/quarterly-earnings",
+                 "json_req":False
+             },
+             {  "Name":"JP Morgan",
+                 "Code":"JPM",
+                 "report_avalaible_from":2007,
+                 "report_avalaible_till":2023,
+                 "url":"https://www.jpmorganchase.com/ir/quarterly-earnings",
+                 "base_url":"https://www.jpmorganchase.com/",
+                 "json_url":"https://www.jpmorganchase.com/services/json/jpmc/ir-service/path.service/type=quarterly-earnings.json",
+                 "json_req":True
              }
 
 ]
 
-HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \ (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \ (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'}
 
 current_year=2024
 
 def get_all_companys_data():
-  all_comany_pdfs={}
+  all_comany_pdfs=defaultdict(lambda:[])
   data=[]
-  for comany in company_list:
-    dom,is_success=get_dom_object(comany["url"],HEADERS)
+
+  for comany in company_list[2:]:
+    obj=etree.HTML(str(BeautifulSoup('', "html.parser")))
+                   
+    if(comany["json_req"]):
+      obj,is_success=get_json_object(comany["json_url"],HEADERS)
+      logger.info(obj)
+    else:
+      obj,is_success=get_dom_object(comany["url"],HEADERS)
+      
     if(is_success):
-      data=get_company_data(dom,comany,current_year)
+      data=get_company_data(obj,comany,current_year,comany["json_req"])
 
     all_comany_pdfs[comany["Code"]]=data
 
@@ -135,3 +204,96 @@ def get_all_companys_data():
 
 
 
+def extract_pages_with_text_pypdf(pdf_url, search_text_thresholds):
+    matching_pages = []
+    complete_text=''
+    try:
+        response = requests.get(pdf_url,headers=HEADERS,stream=True)
+        print(response)
+        if response.status_code != 200:
+            print("Failed to fetch the PDF from the URL.")
+            return matching_pages
+        
+        pdf_content = BytesIO(response.content)
+        
+        pdf_reader = PyPDF2.PdfReader(pdf_content)
+            
+        for page_number in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_number]
+            page_text = page.extract_text().lower()
+
+            
+            highest_threshold = 0
+            s = 0
+            for search_text, threshold in search_text_thresholds:
+                s += threshold
+                if search_text.lower() in page_text:
+                    highest_threshold += 100
+                else:
+                    similarity = fuzz.partial_ratio(search_text.lower(), page_text)
+                    if similarity >= threshold:
+                        highest_threshold += max(threshold, similarity)
+
+                    #print(page_number,similarity,search_text,threshold)
+                    
+            if highest_threshold > s:
+                complete_text+=page_text
+                matching_pages.append(page_number )
+    except:
+        print()
+    
+            
+    return matching_pages,complete_text
+
+
+# Function to save PDF file and extract text
+def extract_pages_with_text_llama_parse(url_path, search_text_thresholds,parse=False):
+    pages_to_save=[]
+    text=""
+
+    # Requesting PDF content from the URL
+    response = requests.get(url_path,headers=HEADERS,stream=True)
+    pdf_data = response.content
+
+    # Initializing PDF reader and writer objects
+    pdf_writer = PyPDF2.PdfWriter()
+    pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_data))
+
+    # Iterating through each page of the PDF
+    for page_number in range(len(pdf_reader.pages)):
+        text = pdf_reader.pages[page_number].extract_text().lower()
+        highest_threshold = 0
+        s=0
+        # Calculating similarity thresholds
+        for search_text, threshold in search_text_thresholds:
+            s+=threshold
+            if search_text.lower() in text:
+                highest_threshold += 100
+            else:
+                similarity = fuzz.partial_ratio(search_text.lower(), text)
+                if similarity >= threshold:
+                    highest_threshold += max(threshold, similarity)
+
+        # Adding page to save if similarity threshold is met
+        if highest_threshold>s:
+            pdf_writer.add_page(pdf_reader.pages[page_number])
+            pages_to_save.append(page_number )
+
+    #print(pages_to_save)
+            
+    # Writing selected pages to a temporary PDF file
+    output_filename = './temp.pdf'
+    with open(output_filename,'wb') as out:
+        pdf_writer.write(out)
+
+    text=""
+
+    
+    # Extracting text from PDF pages if requested
+    if(parse):
+      documents = parser.load_data('./temp.pdf')
+      #print(documents,pages_to_save)  
+      for i in range(len(documents)):
+          text+=documents[i].text[:]
+   
+    return pages_to_save,text
